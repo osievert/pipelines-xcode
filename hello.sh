f845f5ae74f4ff6c0a026fff1001aed867b29b2f
Otto Sievert <osievert@gopro.com>
2019-07-29 11:12:20 -0700

    filePath

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "hello, ${Secret1} , ${secret} , $Secret , $secret , $(Secret) , $(secret) , $env:Secret , $env:secret"
+
+if [[ "${Secret1}" == "42" ]] ; then
+  echo "match!"
+else
+  echo "no match"
+fi
+
+
