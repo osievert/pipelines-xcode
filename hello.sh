479278151240243778b862edf9f1e76878644952
Otto Sievert <osievert@gopro.com>
2019-07-29 12:03:50 -0700

    secure9

--- hello.sh
+++ hello.sh
@@ -10,6 +10,9 @@ else
   echo "no match"
 fi
 
-echo "secure file path: ${downloaded_secure_file.secureFilePath}"
-ls -la ${downloaded_secure_file.secureFilePath}
+#echo "secure file path: ${downloaded_secure_file.secureFilePath}"
+#ls -la ${downloaded_secure_file.secureFilePath}
+
+echo "secure file path: $0, $1 , $2"
+ls -la $0 $1 $2
 
